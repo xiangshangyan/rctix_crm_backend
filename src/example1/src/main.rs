3616a700edca6cd290d398dfcf56c2cce15e8554
yanxiangshang <1097529402@qq.com>
2023-08-10 21:07:12 +0800

    feat：闭包初步

---
+++ src/example1/src/main.rs
@@ -0,0 +1,28 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+struct Cacher<T> where T: Fn(u32) -> u32 {
+    query: T,
+    value: Option<u32>,
+}
+
+impl<T> Cacher<T> where T: Fn(u32) -> u32 {
+    fn new(query: T) -> Cacher<T> {
+        Cacher {
+            query,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            None => {
+                let v = self.query(arg);
+                self.value = Some(v);
+                v
+            }
+            Some(v) => { v }
+        }
+    }
+}
