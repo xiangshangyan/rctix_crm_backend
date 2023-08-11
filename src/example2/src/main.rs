dae5ac4a87711df58e0c78c1bd3364776bb339d2
yanxiangshang <1097529402@qq.com>
2023-08-12 07:39:42 +0800

    feat: test rc commit

--- src/example2/src/main.rs
+++ src/example2/src/main.rs
@@ -1,3 +1,6 @@
+use std::rc::Rc;
+use std::sync::Mutex;
+use std::thread;
 use tokio::time::Duration;
 #[tokio::main]
 async fn main() {
@@ -6,11 +9,36 @@ async fn main() {
     println!("11111111111111");
     // 会休眠两秒钟后继续执行
     async_task_handle.await.unwrap();
-    println!("22222222222222")
+    println!("22222222222222");
+    test_rc();
 }
 
 async fn test_async() {
     println!("Async task start...");
     tokio::time::sleep(Duration::from_secs(2)).await;
     println!("Async task end...")
+}
+
+// async fn test_sync() {
+//     let counter = Mutex::new(0);
+//     let mut handles = vec![];
+//     for _ in 0..10 {
+//         let counter = counter.clone();
+//         let handle = tokio::spawn(async move {
+//             let mut num = counter.lock().unwrap();
+//             *num += 1;
+//         });
+//         handles.push(handle);
+//     }
+//     for handle in handles {
+//         handle.await.unwrap();
+//     }
+//     println!("counter: {}", *counter.lock().unwrap());
+// }
+
+fn test_rc() {
+    let rc = Rc::new(0);
+    println!("the count is : {}", Rc::strong_count(&rc));
+    let rc1 = rc.clone();
+    println!("the count is : {}", Rc::strong_count(&rc1));
 }
