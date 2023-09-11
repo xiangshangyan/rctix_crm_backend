9208b495e34fdfdf1ff977cf8a7c109f40f779ad
yanxiangshang <1097529402@qq.com>
2023-09-11 23:06:50 +0800

    feat: commit

--- src/example7/src/main.rs
+++ src/example7/src/main.rs
@@ -19,5 +19,15 @@ fn main() {
         println!("hello: {}, {:?}, {:?}", x, name2, name3);
     };
 
-    println!("c1: {}, c2: {}, c3: {}, c4: {}, c5: {}, main: {}", size_of_val(&c1), size_of_val(&c2), size_of_val(&c3), size_of_val(&c4), size_of_val(&c5), size_of_val(&main), )
+    println!("c1: {}, c2: {}, c3: {}, c4: {}, c5: {}, main: {}", size_of_val(&c1), size_of_val(&c2), size_of_val(&c3), size_of_val(&c4), size_of_val(&c5), size_of_val(&main), );
+
+    test_fn_once();
+}
+
+fn test_fn_once() {
+    let name = String::from("Tyr");    // 这个闭包啥也不干，只是把捕获的参数返回去
+    let c = move |greeting: String| (greeting, name);
+    let result = c("hello".to_string());
+    println!("result: {:?}", result);    // 无法再次调用
+    // let result = c("hi".to_string());
 }
